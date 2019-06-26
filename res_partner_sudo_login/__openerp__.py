50c04311e6a28aa69afa9c0e925c835dddc27958
Daniel Eriksson <daniel.eriksson@vertel.se>
2019-06-26 11:06:40 +0200

    res_partner_sudo_login: Added new module to enable users to login as customers from backend.

---
+++ res_partner_sudo_login/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2019- Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+'name': 'ResPartner Sudo Login',
+'version': '0.1',
+'summary': 'Login as a customer',
+'category': 'project',
+'description': """Login as a customer.
+
+""",
+'author': 'Vertel AB',
+'website': 'http://www.vertel.se',
+'depends': ['base',],
+'data': ['res_partner_view.xml', 'templates.xml'],
+'installable': True,
+}
