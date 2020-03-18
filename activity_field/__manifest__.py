e518d074f329da200e49a9e9741724a0b6703922
Sebastian Gieser <sebgi@odoo12.network4>
2020-03-18 15:20:30 +0000

    activity_field: testing manifest changes

--- activity_field/__manifest__.py
+++ activity_field/__manifest__.py
@@ -31,7 +31,7 @@ This module adds adds a tab with different activities
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'website': 'http://www.vertel.se',
-    'depends': ['base','event'],
+    'depends': ['base','website_event', 'event'],
     'data': [
 			'views/res_partner.xml',
             'security/ir.model.access.csv'
