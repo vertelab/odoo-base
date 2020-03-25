b440444f6bccc06630d7ef84cd300355127d6916
Sebastian Gieser <sebgi@odoo12.network4>
2020-03-25 10:21:35 +0000

    partner_job: initial commit.

---
+++ partner_job/models/res_partner.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2019 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import models, fields, api, _
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResPartner(models.Model):
+    _inherit = "res.partner" 
+    
+
+class Jobs(models.Model):
+    _name = 'res.partner.job'
+
+    partner_id = fields.Many2one(comodel_name="res.partner")
+    
+    name = fields.Char(string="Job")
+    job_id = fields.Char(string="Job ID")
+    description = fields.Text(string="Job description")
+    ssyk_ids = fields.Many2many('res.ssyk', string="SSYK") 
+    
