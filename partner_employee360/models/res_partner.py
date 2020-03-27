1842ba95c3ded8737d27195ef7793062f512a794
Sebastian Gieser <sebgi@odoo12.network4>
2020-03-27 12:51:09 +0000

    partner_employee360 now contains all code for offices and is fully working.

--- partner_employee360/models/res_partner.py
+++ partner_employee360/models/res_partner.py
@@ -26,30 +26,26 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = "res.partner" #odoo inheritance från res.partner
     #_name = ""
-    office = fields.Many2one('res.partner.office', string="Office")
-    work_phone = fields.Integer(string='Work phone', help="Work phone number")
-    available_since = fields.Datetime(string='Available since', help="Time when they became available") #datetime/time?
-    org_or_social_sec_nr = fields.Char(string='Organization/Social security number', help="Social security number or organization number")
-    cfar = fields.Integer(string='CFAR', help="CFAR number")
-    customer_nr = fields.Integer(string='Customer number', help="Customer number")
-
-    office_code = fields.Integer(string='Office code', related='office.office_code')
     
-    #office_code = fields.Integer(string="Office code") #bör tas från office och vara satt för office partners
+    work_phone = fields.Integer(string='Work phone', help="Work phone number")
+    org_or_social_sec_nr = fields.Char(string='Organization/Social security number', help="Social security number or organization number")
+    cfar = fields.Char(string='CFAR', help="CFAR number")
+    customer_nr = fields.Char(string='Customer number', help="Customer number")
+
+    #office selection field for partners connected to an office, my_office_code filled in by office_code for the office
+    office = fields.Many2one('res.partner', string="Office")
+    my_office_code = fields.Char(string='Office code', related='office.office_code')
+    
+    #adds af office as a type of partner
+    type = fields.Selection(selection_add=[('af office', 'AF Office')])
+    
+    #office code for office type partners only
+    office_code = fields.Char(string="Office code")
+    
     is_jobseeker = fields.Boolean(string="Jobseeker")
     is_independent_partner = fields.Boolean(string="Independent partner")
     is_government = fields.Boolean(string="Government")
     is_employer = fields.Boolean(string="Employer")
-
-    type = fields.Selection(selection_add=[('af office', 'AF Office')])
-
-class Office(models.Model):
-    _name = "res.partner.office"
-    
-
-    name = fields.Many2one('res.partner', sting="Office")
-    office_code = fields.Integer(string="Office code") 
-    
 
     
 
