07b84a90c2b0311b5ad237ff6a0a4371fc30c833
Sebastian Gieser <sebgi@odoo12.network4>
2020-04-06 13:13:59 +0000

    res_drivers_license AFC-197: minor code fixes

--- res_drivers_license/models/res_drivers_license.py
+++ res_drivers_license/models/res_drivers_license.py
@@ -25,9 +25,9 @@ _logger = logging.getLogger(__name__)
 class ResDriversLicense(models.Model): 
     _name = 'res.drivers_license'
 
-    partner_id = fields.Many2many(comodel_name="res.partner")
+    partner_id = fields.One2many(comodel_name="res.partner", inverse_name="drivers_license_id")
     
-    name = fields.Char(string='Type', required=True) #A,B etc.
+    name = fields.Char(string='Class', required=True) #A,B etc.
     description = fields.Char(string='Description')
 
 
