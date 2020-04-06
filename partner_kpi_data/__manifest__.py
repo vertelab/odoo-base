7de07768ab5b70835e2b76c11e707bb419704c3f
Sebastian Gieser <sebgi@odoo12.network4>
2020-04-06 12:32:15 +0000

    partner_kpi_data AFC-84: untested code, access issue

---
+++ partner_kpi_data/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Partner KPI',
+    'version': '12.0.1.1',						
+    'category': '',
+    'description': """
+KPI 
+===========================================================
+AFC-84
+""",
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': ['base'],
+    'data': [
+			'views/res_partner.xml',
+            #'security/ir.model.access.csv'
+        ],
+    'application': False,
+    'installable': True,
+}
