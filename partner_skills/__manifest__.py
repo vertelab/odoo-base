ec688dfe0f6b2bf330c6c7d2cd2a95c10f425b34
Sebastian Gieser <sebgi@odoo12.network4>
2020-04-17 15:04:02 +0000

    partner_skills AFC-106: updated manifest

--- partner_skills/__manifest__.py
+++ partner_skills/__manifest__.py
@@ -27,13 +27,14 @@
 Adds skills to the partner view
 ================================================================================================
 Gets skills from hr_skill
+AFC-106
 """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'website': 'http://www.vertel.se',
     'depends': ['base', 'hr_skill'],
     'data': [
-			'views/res_partner.xml'
+			'views/res_partner_view.xml'
         ],
     'application': False,
     'installable': True,
