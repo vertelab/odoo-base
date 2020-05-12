5267caa734f8b3a074b63d8c7890cd27695003f6
Sebastian Gieser <sebgi@odoo12.network4>
2020-05-12 14:06:28 +0000

    partner_view_360, partner_daily_notes, partner_kpi_data, AFC-259, AFC-260: Jobseeker contact card city field, computed age field. Employer contact card now has KPI tab Daily notes menu fixed

--- partner_view_360/models/res_partner.py
+++ partner_view_360/models/res_partner.py
@@ -21,6 +21,7 @@
 
 from odoo import models, fields, api, _
 import logging
+from datetime import date
 _logger = logging.getLogger(__name__)
 
 
@@ -29,7 +30,7 @@ class ResPartner(models.Model):
     #_name = ""
 
     work_phone = fields.Integer(string='Work phone', help="Work phone number")
-    social_security_nr = fields.Char(string="Social security number", related="company_registry")
+    age = fields.Char(string="Age", compute="calculate_age")
     company_registry = fields.Char(
         string='Organization number', help="organization number")
     cfar = fields.Char(string='CFAR', help="CFAR number")
@@ -59,12 +60,58 @@ class ResPartner(models.Model):
     foreign_country_of_work = fields.Char(string="When working in foreign country")
     deactualization_message = fields.Text(string="Message to jobseeker regarding deactualization")
 
+    state_statecode = fields.Char(compute="combine_state_statecode")
+
     registered_through = fields.Char(string="Registered Through")
     share_info_with_employers = fields.Boolean(string="Share name and address with employers")
     sms_reminders = fields.Boolean(string="SMS reminders")
     postal_address_id = fields.Many2one('res.partner', string="Postal address")
-    postal_address_street = fields.Char(string="Postal address", related="postal_address.street")
-    postal_address_zip = fields.Char(related="postal_address.zip")
-    postal_address_city = fields.Char(related="postal_address.city")
+    postal_address_street = fields.Char(string="Postal address", related="postal_address_id.street")
+    postal_address_zip = fields.Char(related="postal_address_id.zip")
+    postal_address_city = fields.Char(related="postal_address_id.city")
 
-    #postal_address = fields.Char('res.partner')
+    #def combine_state_statecode(self):
+        
+
+    def calculate_age(self):
+        wrong_input = False
+        today = date.today()
+        if len(self.company_registry.split("-")[1]) != 4:
+            wrong_input = True
+        social_sec_stripped = self.company_registry[:-4]
+        social_sec_stripped = social_sec_stripped.split("-")[0]
+        date_of_birth = date(1980,1,1)
+        if len(social_sec_stripped) == 6:
+            yr = social_sec_stripped[:2]
+            year = int("20"+yr)
+            month = int(social_sec_stripped[2:4])
+            day = int(social_sec_stripped[4:6])
+            try:
+                date_of_birth = date(year, month, day)
+            except:
+                wrong_input = True
+                _logger.error("Could not convert social security number (company_registry) to date")
+            if today.year - date_of_birth.year < 18:
+                year = int("19"+yr)
+                try:
+                    date_of_birth = date(year, month, day)
+                except:
+                    wrong_input = True
+                    _logger.error("Could not convert social security number (company_registry) to date")
+    
+        elif len(social_sec_stripped) > 6:
+            try:
+                date_of_birth = date(int(social_sec_stripped[:4]),int(social_sec_stripped[4:6]),int(social_sec_stripped[6:8]))
+            except:
+                wrong_input = True
+                _logger.error("Could not convert social security number (company_registry) to date")
+        else: 
+            wrong_input = True
+        
+        if not wrong_input:
+            years = today.year - date_of_birth.year
+            if today.month < date_of_birth.month or (today.month == date_of_birth.month and today.day < date_of_birth.day):
+                years -= 1
+            self.age = years
+        else: 
+            self.age = "Error calculating age"
