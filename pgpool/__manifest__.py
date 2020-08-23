fbc8125ec4e76843f4813bdc7805776a902700bd
Robin Chatfield <Robin Chatfield>
2020-08-23 10:16:11 +0200

    AFC-670, pgpool: Module created.

---
+++ pgpool/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'PGPool',
+    'version': '12.0.0.1',
+    'category': '',
+    'description': """
+Adds support for PGPool-II clusters.
+This module must be loaded server wide through the config file.
+
+Example:
+server_wide_modules = base,web,pgpool
+""",
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': [],
+    'data': [],
+    'application': False,
+    'installable': False,
+}
