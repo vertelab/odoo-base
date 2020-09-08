0da2cf376e6ae6d08a70654b2ab99460fdba3a9b
Mikael Holm <mikael.holm@arbetsformedlingen.se>
2020-09-08 13:03:53 +0200

    adjust_errormsg_time 12.0-AFC-813: Added module

---
+++ adjust_errormsg_time/static/src/js/attach_document.js
@@ -0,0 +1,18 @@
+odoo.define('adjust_errormsg_time.AttachDocument', function (require) {
+    "use strict";
+    var AttachDocument = require('web.AttachDocument');
+
+    AttachDocument.include({
+        _notifyInvalidFields: function (invalidFields) {
+            var fields = this.state.fields;
+            var warnings = invalidFields.map(function (fieldName) {
+                var fieldStr = fields[fieldName].string;
+                return _.str.sprintf('<li>%s</li>', _.escape(fieldStr));
+            });
+            warnings.unshift('<ul>');
+            warnings.push('</ul>');
+            this.do_warn(_t("The following fields are invalid:"), warnings.join(''), true);
+         },
+
+    });
+});
