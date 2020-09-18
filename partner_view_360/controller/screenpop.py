7d249a3cfc1969e3dd938f978159b907cbc762c0
Daniel Eriksson <daniel.eriksson@arbetsformedlingen.se>
2020-09-18 16:05:52 +0200

    partner_view_360: updated screenpop-url

--- partner_view_360/controller/screenpop.py
+++ partner_view_360/controller/screenpop.py
@@ -76,9 +76,9 @@ class WebsiteScreenpop(http.Controller):
         elif partner and len(partner) == 1:
             action = request.env.ref('partner_view_360.action_jobseekers')
             partner.eidentification = post.get('bankid')
-            res_url = '%s/web?id=%s&action=%s&model=res.partner&view_type=form#id=%s&active_id=40&model=res.partner&view_type=form' % (
+            res_url = '%s/web#id=%s&action=%s&model=res.partner&view_type=form' % (
                                                                 request.env['ir.config_parameter'].sudo().get_param('web.base.url',''),
-                                                                partner.id if partner else 0,action.id if action else 0,partner.id if partner else 0
+                                                                partner.id if partner else 0,action.id if action else 0
                                                             )            
             if post.get('bankid') != 'OK':
                 return request.render('partner_view_360.bankid', {
