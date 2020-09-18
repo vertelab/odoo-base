5335ae92cf86202eb06881653672b7fca578d3db
Daniel Eriksson <daniel.eriksson@arbetsformedlingen.se>
2020-09-18 11:44:32 +0200

    partner_view_360: re-added vital code.

--- partner_view_360/models/res_partner.py
+++ partner_view_360/models/res_partner.py
@@ -46,6 +46,31 @@ class ResPartner(models.Model):
     customer_id = fields.Char(string='Customer number', help="Customer number")
     eidentification = fields.Char(string='E-Identification', help="BankId or other e-identification done OK or other")
 
+    # office selection field for partners connected to an office, my_office_code filled in by office_code for the office
+    office = fields.Many2one('res.partner', string="Office") #should check for type = "af office"
+
+
+    # office_ids is a better name, One2many need a help-class that cannot be res.partner with a inverse_name
+    # ~ office_campuses = fields.One2many('res.partner', related="office.campuses") 
+    # ~ my_campuses = fields.One2many('res.partner') #should check if if it's in office_locations
+
+
+    #office_ids = fields.Many2many('res.partner', relation='res_partner_office_partner_rel', column1='partner_id', column2='office_id', string='Offices')
+    my_office_code = fields.Char(string='Office code', related='office.office_code')
+
+    # adds af office as a type of partner
+    type = fields.Selection(selection_add=[('af office', 'AF Office'), ('foreign address','Foreign Address'), ('given address','Given address'), ('visitation address','Visitation Address'), ('campus', 'Campus'), ('mailing address', 'Mailing Address')])
+
+    # office code for office type partners only
+    office_code = fields.Char(string="Office code")
+
+    # campus_ids is a better name, One2many need a help-class that cannot be res.partner with a inverse_name
+    # ~ campuses = fields.One2many('res.partner', string="Campuses") # should check for type = "campus"
+
+    # Location code for campus type partners only
+    location_code = fields.Char(string="Location Code") 
+    work_place_code = fields.Char(string="Work place code")
+
     is_jobseeker = fields.Boolean(string="Jobseeker")
     is_independent_partner = fields.Boolean(string="Independent partner")
     is_government = fields.Boolean(string="Government")
