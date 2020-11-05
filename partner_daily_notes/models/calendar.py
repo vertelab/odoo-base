20dd6d3f653177b42f150d94457355bc8b3989e5
Sebastian Gieser <sebastian.gieser@vertel.se>
2020-11-05 16:44:19 +0100

    partner_daily_notes: auto daily notes should now find the current logged in user instead of superuser and and even if it doesn't work right it won't crash

--- partner_daily_notes/models/calendar.py
+++ partner_daily_notes/models/calendar.py
@@ -21,6 +21,7 @@
 
 from odoo import models, fields, api, _
 import logging
+from odoo import SUPERUSER_ID
 _logger = logging.getLogger(__name__)
 
 class CalendarAppointment(models.Model):
@@ -32,10 +33,10 @@ class CalendarAppointment(models.Model):
         vals = {
             "name": _("Cancelled %s. Reason: %s" % (self.type_id.name, cancel_reason.name)),
             "partner_id": self.partner_id.id,
-            "administrative_officer": self.env.user.id,
+            "administrative_officer": env.context.get('uid') if env.context.get('uid') != SUPERUSER_ID else False,
             "note": _("Cancelled %s: %s. Reason: %s" % (self.type_id.name, self.start, cancel_reason.name)) if self.channel_name == "PDM" else _("Cancelled %s: %s, %s %s. Reason: %s" % (self.type_id.name, self.start, self.office_id.office_code, self.user_id.login, cancel_reason.name)),
             "note_type": self.env.ref('partner_daily_notes.note_type_as_02').id,
-            "office_id": self.env.user.office_ids._ids[0],
+            "office_id": self.env.user.office_ids._ids[0] if self.env.user.office_ids else False,
             "note_date": self.start,
             "appointment_id": self.id,
         }
@@ -49,10 +50,10 @@ class CalendarAppointment(models.Model):
         vals = {
             "name": _("Meeting moved %s. Reason: %s" % (self.type_id.name, reason.name)),
             "partner_id": self.partner_id.id,
-            "administrative_officer": self.env.user.id,
+            "administrative_officer": env.context.get('uid') if env.context.get('uid') != SUPERUSER_ID else False,
             "note": _("Meeting moved %s: %s. Reason: %s" % (self.type_id.name, self.start, reason.name)) if self.channel_name == "PDM" else _("Meeting moved %s: %s, %s %s. Reason: %s" % (self.type_id.name, self.start, self.office_id.office_code, self.user_id.login, reason.name)),
             "note_type": self.env.ref('partner_daily_notes.note_type_as_02').id,
-            "office_id": self.env.user.office_ids._ids[0],
+            "office_id": self.env.user.office_ids._ids[0] if self.env.user.office_ids else False,
             "appointment_id": self.id,
         }
         self.partner_id.sudo().notes_ids = [(0, 0, vals)]
@@ -70,10 +71,10 @@ class CalendarAppointment(models.Model):
             vals = {
                     "name": _("Booked %s" % self.type_id.name),
                     "partner_id": self.partner_id.id,
-                    "administrative_officer": self.env.user.id,
+                    "administrative_officer": env.context.get('uid') if env.context.get('uid') != SUPERUSER_ID else False,
                     "note":_("Booked %s: %s." % (self.type_id.name, self.start)) if self.channel_name == "PDM" else _("Booked %s: %s, %s %s." % (self.type_id.name, self.start, self.office_id.office_code, self.user_id.login)),
                     "note_type": self.env.ref('partner_daily_notes.note_type_as_02').id,
-                    "office_id": self.env.user.office_ids._ids[0],
+                    "office_id": self.env.user.office_ids._ids[0] if self.env.user.office_ids else False,
                     "note_date": self.start,
                     "appointment_id": self.id,
                 }
@@ -93,10 +94,10 @@ class CalendarAppointmentSuggestion(models.Model):
         vals = {
                 "name": _("Booked %s" % self.appointment_id.type_id.name),
                 "partner_id": self.appointment_id.partner_id.id,
-                "administrative_officer": self.env.user.id,
+                "administrative_officer": env.context.get('uid') if env.context.get('uid') != SUPERUSER_ID else False,
                 "note":_("Booked %s: %s." % (self.appointment_id.type_id.name, self.appointment_id.start)) if self.appointment_id.channel_name == "PDM" else _("Booked %s: %s, %s %s." % (self.appointment_id.type_id.name, self.appointment_id.start, self.appointment_id.office_id.office_code, self.appointment_id.user_id.login)),
                 "note_type": self.env.ref('partner_daily_notes.note_type_as_02').id,
-                "office_id": self.env.user.office_ids._ids[0],
+                "office_id": self.env.user.office_ids._ids[0] if self.env.user.office_ids else False,
                 "note_date": self.appointment_id.start,
                 "appointment_id": self.appointment_id.id,
             }
