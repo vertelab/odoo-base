51d3a735ba3c78500eb0fe16a1e70d591076fe63
Artur Chilangwa <artur.chilangwa@vertel.se>
2020-11-19 13:31:22 +0100

    partner_test-data_fenix AFC-1306: Add code moved from fenix_test_contacts
    This module is only a rename of fenix_test_contacts

---
+++ partner_test-data_fenix/__manifest__.py
@@ -0,0 +1,22 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fenix Contacts Test Data",
+    "version": "12.0.1.0.1",
+    "author": "Vertel AB",
+    "license": "AGPL-3",
+    "website": "https://vertel.se/",
+    "category": "Tools",
+    "depends": [
+        "base",
+        "contacts",
+    ],
+    "external_dependencies": [],
+    "data": [
+        'data/res_company.xml',
+        'data/res_partner.xml',
+        'data/res_user.xml',
+    ],
+    "application": True,
+    "installable": True,
+}
