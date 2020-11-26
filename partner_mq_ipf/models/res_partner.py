453ade6f9d843af6e60eae651b0c6208af9defb3
Daniel Eriksson <daniel.eriksson@arbetsformedlingen.se>
2020-11-26 11:57:08 +0100

    partner_mq_ipf AFC-1360: pep8 formatted

--- partner_mq_ipf/models/res_partner.py
+++ partner_mq_ipf/models/res_partner.py
@@ -32,22 +32,25 @@ import threading
 
 _logger = logging.getLogger(__name__)
 
+
 def subscribe(mqconn, target, clientid=4):
     mqconn.subscribe(destination=target, clientid=clientid, ack="client")
+
 
 def connect_and_subscribe(mqconn, user, pwd, target, clientid=4):
     mqconn.connect(user, pwd, wait=True)
     subscribe(mqconn, target, clientid=clientid)
 
-PREN="prenumerera-arbetssokande"
-PNR="personnummer"
-PREVPNR="tidigarePersonnummer"
-SID="sokandeId"
-MSGTYPE="meddelandetyp"
-TIMESTAMP="tidpunkt"
+
+PREN = "prenumerera-arbetssokande"
+PNR = "personnummer"
+PREVPNR = "tidigarePersonnummer"
+SID = "sokandeId"
+MSGTYPE = "meddelandetyp"
+TIMESTAMP = "tidpunkt"
+
 
 class AsokResPartnerListener(stomp.ConnectionListener):
-
     def __init__(self, mqconn, user, pwd, target, clientid=4):
         self.__conn = mqconn
         self.__user = user
@@ -61,7 +64,7 @@ class AsokResPartnerListener(stomp.ConnectionListener):
         try:
             xmldict = xmltodict.parse(message, dict_constructor=dict)
             # Validate xml dict
-            _logger.debug('__parse_message: %s' % xmldict)
+            _logger.debug("__parse_message: %s" % xmldict)
             if (
                 not isinstance(xmldict, dict)
                 or not PREN in xmldict.keys()
@@ -83,7 +86,7 @@ class AsokResPartnerListener(stomp.ConnectionListener):
 
     def _handle_message(self, headers, message):
         data = self.__parse_message(message)
-        _logger.debug('_handle_message: %s' % data)
+        _logger.debug("_handle_message: %s" % data)
         if data:
             # Add message to list
             self.__msglist.append((headers, data))
@@ -102,11 +105,11 @@ class AsokResPartnerListener(stomp.ConnectionListener):
         :param body: the frame's payload - usually a detailed error description.
         """
         _logger.error("Asok MQ Listener error: %s - %s" % (headers, body))
- 
+
     def on_message(self, headers, msg):
         _logger.debug("Asok MQ Listener on_message: {0} - {1}".format(headers, msg))
         self._handle_message(headers, msg)
-    
+
     def ack_message(self, msg):
         headers, body = msg
         # tell MQ we handled the message
@@ -114,7 +117,9 @@ class AsokResPartnerListener(stomp.ConnectionListener):
 
     def on_disconnected(self):
         # Probably happened because we asked to disconnect
-        _logger.warning('Asok MQ Listener disconnected from MQ - NOT Trying to reconnect')
+        _logger.warning(
+            "Asok MQ Listener disconnected from MQ - NOT Trying to reconnect"
+        )
 
     def on_connecting(self, host_and_port):
         """
@@ -140,9 +145,10 @@ class AsokResPartnerListener(stomp.ConnectionListener):
         """
         _logger.debug("Asok MQ Listener on_connected: %s - %s" % (headers, body))
 
+
 class STOMResPartnerListener(stomp.ConnectionListener):
     __conn = None
-    __user = None 
+    __user = None
     __pwd = None
     __target = None
     __clientid = None
@@ -189,17 +195,17 @@ class STOMResPartnerListener(stomp.ConnectionListener):
         :param body: the frame's payload - usually a detailed error description.
         """
         _logger.error("STOM MQ Listener error: %s - %s" % (headers, body))
- 
+
     def on_message(self, headers, msg):
         _logger.debug("STOM MQ Listener on_message: {0} - {1}".format(headers, msg))
         self._handle_message(msg)
         # tell MQ we handled the message
         # TODO: Should probably move ack to successful message handling. See AsokResPartnerListener
         self.__conn.ack(headers["message-id"])
-    
+
     def on_disconnected(self):
         # Probably happened because we asked to disconnect
-        _logger.warning('STOM MQ Listener disconnected from MQ - Tring to reconnect')
+        _logger.warning("STOM MQ Listener disconnected from MQ - Tring to reconnect")
 
     def on_connecting(self, host_and_port):
         """
@@ -225,14 +231,17 @@ class STOMResPartnerListener(stomp.ConnectionListener):
         """
         _logger.debug("STOM MQ Listener on_connected: %s - %s" % (headers, body))
 
+
 class ResPartner(models.Model):
-    _inherit = "res.partner"  
+    _inherit = "res.partner"
 
     def __get_host_port(self):
-        str = self.env['ir.config_parameter'].get_param('partner_mq_ipf.mqhostport', '172.16.36.27:61613')
+        str = self.env["ir.config_parameter"].get_param(
+            "partner_mq_ipf.mqhostport", "172.16.36.27:61613"
+        )
         hosts = list()
-        for vals in str.split(','):
-            hosts.append(tuple(vals.split(':')))
+        for vals in str.split(","):
+            hosts.append(tuple(vals.split(":")))
 
         return hosts
 
@@ -245,7 +254,7 @@ class ResPartner(models.Model):
             env_new = api.Environment(new_cr, uid, context)
             # passing a bool in do_list to indicate if we should keep
             # running this loop. What is a better solution?
-            while(do_list[0]):
+            while do_list[0]:
                 message = False
                 if msg_list:
                     try:
@@ -257,16 +266,23 @@ class ResPartner(models.Model):
                             customer_id = msg.get(SID)
                             social_security_number = msg.get(PNR)
                             former_social_security_number = msg.get(PREVPNR, None)
-                            message_type = msg.get(MSGTYPE) 
+                            message_type = msg.get(MSGTYPE)
                             # Send request to RASK
-                            env_new['res.partner'].rask_controller(customer_id, social_security_number, former_social_security_number, message_type)
-                            # append message to ack_list to let main 
+                            env_new["res.partner"].rask_controller(
+                                customer_id,
+                                social_security_number,
+                                former_social_security_number,
+                                message_type,
+                            )
+                            # append message to ack_list to let main
                             # thread know that this can be ACK'd
                             with lock:
                                 ack_list.append(message)
                         message = False
                     except:
-                        _logger.exception('Asok MQ Sender: error sending request to RASK!')
+                        _logger.exception(
+                            "Asok MQ Sender: error sending request to RASK!"
+                        )
                 else:
                     # slow down so we don't loop all the time
                     time.sleep(1)
@@ -274,49 +290,56 @@ class ResPartner(models.Model):
         env_new.cr.close()
 
     @api.model
-    def mq_asok_listener(self, minutes_to_live = 10): 
+    def mq_asok_listener(self, minutes_to_live=10):
         _logger.info("Asok MQ Listener started.")
         host_port = self.__get_host_port()
-        target = self.env['ir.config_parameter'].get_param('partner_mq_ipf.target_asok', '/topic/Consumer.crm.VirtualTopic.arbetssokande.andring')
-        usr = self.env['ir.config_parameter'].get_param('partner_mq_ipf.mquser', 'crm')
-        pwd = self.env['ir.config_parameter'].get_param('partner_mq_ipf.mqpwd', 'topsecret')
-        stomp_log_level = self.env['ir.config_parameter'].get_param('partner_mq_ipf.stomp_logger', 'INFO')
+        target = self.env["ir.config_parameter"].get_param(
+            "partner_mq_ipf.target_asok",
+            "/topic/Consumer.crm.VirtualTopic.arbetssokande.andring",
+        )
+        usr = self.env["ir.config_parameter"].get_param("partner_mq_ipf.mquser", "crm")
+        pwd = self.env["ir.config_parameter"].get_param(
+            "partner_mq_ipf.mqpwd", "topsecret"
+        )
+        stomp_log_level = self.env["ir.config_parameter"].get_param(
+            "partner_mq_ipf.stomp_logger", "INFO"
+        )
 
         # decide the stomper log level depending on param
-        stomp_logger = logging.getLogger('stomp.py')
+        stomp_logger = logging.getLogger("stomp.py")
         stomp_logger.setLevel(getattr(logging, stomp_log_level))
 
         mqconn = stomp.Connection10(host_port)
 
-        if self.env['ir.config_parameter'].get_param('partner_mq_ipf.mqusessl', '1') == '1':
+        if (
+            self.env["ir.config_parameter"].get_param("partner_mq_ipf.mqusessl", "1")
+            == "1"
+        ):
             mqconn.set_ssl(for_hosts=host_port, ssl_version=ssl.PROTOCOL_TLS)
             _logger.debug("Asok MQ Listener - Using TLS")
         else:
             _logger.debug("Asok MQ Listener - Not using TLS")
 
         respartnerlsnr = AsokResPartnerListener(mqconn, usr, pwd, target, 4)
-        mqconn.set_listener('', respartnerlsnr)
+        mqconn.set_listener("", respartnerlsnr)
 
         try:
-            connect_and_subscribe(
-                mqconn,
-                usr,
-                pwd,
-                target
-            )
+            connect_and_subscribe(mqconn, usr, pwd, target)
 
-            counter = 12 * minutes_to_live # Number of 5 sek slices to wait
+            counter = 12 * minutes_to_live  # Number of 5 sek slices to wait
             # define common lists to be used between threads
             msg_list = []
             ack_list = []
             # passing a bool in do_list to indicate if we should keep
             # running mq_asok_sender. What is a better solution?
             do_list = [True]
-            # define a lock so we can lock msg_list while updating it 
+            # define a lock so we can lock msg_list while updating it
             # in each thread.
             lock = threading.Lock()
             # create a new thread running mq_asok_sender function
-            sender_thread = threading.Thread(target=self.mq_asok_sender, args=(msg_list, ack_list, do_list, lock))
+            sender_thread = threading.Thread(
+                target=self.mq_asok_sender, args=(msg_list, ack_list, do_list, lock)
+            )
             sender_thread.start()
 
             processed_list = []
@@ -324,35 +347,52 @@ class ResPartner(models.Model):
             while counter > 0:
                 # Let messages accumulate
                 time.sleep(5)
-                _logger.debug('__msglist before unsubscribe: %s' % respartnerlsnr.get_list())
+                _logger.debug(
+                    "__msglist before unsubscribe: %s" % respartnerlsnr.get_list()
+                )
                 # check if we have ACKs to send out
                 with lock:
-                    while(len(ack_list) > 0):
+                    while len(ack_list) > 0:
                         try:
                             # read ack_list and send ACK to MQ queue
                             ack_message = ack_list.pop()
-                            # use a generator to find the matching message from current list 
-                            # not sure if this is actually needed but
-                            # I've implemented it to stop problems with
-                            # us ACKing messages that disapear...
-                            ack_message_current = next((msg for msg in respartnerlsnr.get_list() if msg[0]["message-id"] == ack_message[0]['message-id']), False)
+                            # use a generator to find the matching 
+                            # message from current list not sure if this 
+                            # is actually needed but I've implemented it
+                            # to stop problems with us ACKing messages 
+                            # that disapear...
+                            ack_message_current = next(
+                                (
+                                    msg
+                                    for msg in respartnerlsnr.get_list()
+                                    if msg[0]["message-id"]
+                                    == ack_message[0]["message-id"]
+                                ),
+                                False,
+                            )
                             if not ack_message_current:
                                 # this message ghosted us????
-                                _logger.warn("Asok MQ Listener - COULD NOT FIND MESSAGE TO ACC: %s" % ack_message[0]['message-id'])
+                                _logger.warn(
+                                    "Asok MQ Listener - COULD NOT FIND MESSAGE TO ACC: %s"
+                                    % ack_message[0]["message-id"]
+                                )
                             else:
-                                _logger.debug("Asok MQ Listener - ACK: %s" % ack_message_current[0]['message-id'])
+                                _logger.debug(
+                                    "Asok MQ Listener - ACK: %s"
+                                    % ack_message_current[0]["message-id"]
+                                )
                                 respartnerlsnr.ack_message(ack_message_current)
                             ack_message_current = False
                             ack_message = False
                         except:
-                            _logger.exception('Asok MQ Listener: error ACK')
+                            _logger.exception("Asok MQ Listener: error ACK")
                 # Stop listening
                 mqconn.unsubscribe(target)
 
                 # Handle list of messages
                 for message in respartnerlsnr.get_list():
                     _logger.debug("Asok MQ Listener - adding message to internal queue")
-                    if message[0]['message-id'] not in processed_list:
+                    if message[0]["message-id"] not in processed_list:
                         try:
                             with lock:
                                 msg_list.append(message)
@@ -360,16 +400,20 @@ class ResPartner(models.Model):
                             # track of what messages have been processed
                             # this is needed since we will recieve already
                             # processed messages until they are ACK'd
-                            processed_list.append(message[0]['message-id'])
+                            processed_list.append(message[0]["message-id"])
                         except:
-                            _logger.exception('Asok MQ Listener: error adding message to internal queue')
+                            _logger.exception(
+                                "Asok MQ Listener: error adding message to internal queue"
+                            )
                 message = False
                 self.env.cr.commit()
                 # Clear accumulated messages
                 respartnerlsnr.clear_list()
                 # Check if stop has been called
-                cronstop = self.env['ir.config_parameter'].get_param('partner_mq_ipf.cronstop', '0')
-                if cronstop == '0':
+                cronstop = self.env["ir.config_parameter"].get_param(
+                    "partner_mq_ipf.cronstop", "0"
+                )
+                if cronstop == "0":
                     counter -= 1
                     if counter > 0:
                         # Only subscribe if we haven't reached the end yet
@@ -385,63 +429,71 @@ class ResPartner(models.Model):
             if mqconn.is_connected():
                 mqconn.disconnect()
 
-    def mq_stom_listener(self, minutes_to_live = 10): 
+    def mq_stom_listener(self, minutes_to_live=10):
         _logger.info("STOM MQ Listener started.")
         host_port = self.__get_host_port()
-        target = self.env['ir.config_parameter'].get_param('partner_mq_ipf.target_STOM', '/topic/Consumer.crm.VirtualTopic.arbetssokande.andring')
-        usr = self.env['ir.config_parameter'].get_param('partner_mq_ipf.mquser', 'crm')
-        pwd = self.env['ir.config_parameter'].get_param('partner_mq_ipf.mqpwd', 'topsecret')
-        stomp_log_level = self.env['ir.config_parameter'].get_param('partner_mq_ipf.stomp_logger', 'INFO')
+        target = self.env["ir.config_parameter"].get_param(
+            "partner_mq_ipf.target_STOM",
+            "/topic/Consumer.crm.VirtualTopic.arbetssokande.andring",
+        )
+        usr = self.env["ir.config_parameter"].get_param("partner_mq_ipf.mquser", "crm")
+        pwd = self.env["ir.config_parameter"].get_param(
+            "partner_mq_ipf.mqpwd", "topsecret"
+        )
+        stomp_log_level = self.env["ir.config_parameter"].get_param(
+            "partner_mq_ipf.stomp_logger", "INFO"
+        )
 
         # decide the stomper log level depending on param
-        stomp_logger = logging.getLogger('stomp.py')
+        stomp_logger = logging.getLogger("stomp.py")
         stomp_logger.setLevel(getattr(logging, stomp_log_level))
 
-        _logger.debug("STOM MQ Listener patameters - %s %s - %s %s" % ([host_port], target, usr, pwd))
+        _logger.debug(
+            "STOM MQ Listener patameters - %s %s - %s %s"
+            % ([host_port], target, usr, pwd)
+        )
 
         mqconn = stomp.Connection10(host_port)
-        
-        if self.env['ir.config_parameter'].get_param('partner_mq_ipf.mqusessl', '1') == '1':
+
+        if (
+            self.env["ir.config_parameter"].get_param("partner_mq_ipf.mqusessl", "1")
+            == "1"
+        ):
             mqconn.set_ssl(for_hosts=host_port, ssl_version=ssl.PROTOCOL_TLS)
             _logger.debug("STOM MQ Listener - Using TLS")
         else:
             _logger.debug("STOM MQ Listener - Not using TLS")
 
-        respartnerlsnr = STOMResPartnerListener(mqconn,usr,pwd,target,4)
-        mqconn.set_listener('', respartnerlsnr)
-        
+        respartnerlsnr = STOMResPartnerListener(mqconn, usr, pwd, target, 4)
+        mqconn.set_listener("", respartnerlsnr)
 
         try:
-            connect_and_subscribe(
-                mqconn,
-                usr,
-                pwd,
-                target
-            )
-            counter = 12 * minutes_to_live # Number of 5 sek slices to wait
+            connect_and_subscribe(mqconn, usr, pwd, target)
+            counter = 12 * minutes_to_live  # Number of 5 sek slices to wait
             while counter > 0:
-                time.sleep(5) # wait for a bit
+                time.sleep(5)  # wait for a bit
                 mqconn.unsubscribe(target)
 
                 # handle list of messages
                 for msg in respartnerlsnr.get_list():
                     _logger.info("STOM MQ Listener - calling send_to_stom_track")
                     _logger.debug("STOM MQ Listener - send_to_stom_track: %s" % msg)
-                    self.env['res.partner'].send_to_stom_track(msg)
+                    self.env["res.partner"].send_to_stom_track(msg)
 
                 self.env.cr.commit()
                 respartnerlsnr.clear_list()
-                cronstop = self.env['ir.config_parameter'].get_param('partner_mq_ipf.cronstop', '0')
+                cronstop = self.env["ir.config_parameter"].get_param(
+                    "partner_mq_ipf.cronstop", "0"
+                )
 
-                if cronstop == '0':
+                if cronstop == "0":
                     mqconn.subscribe(target)
                     counter -= 1
                 else:
                     counter = 0
 
         finally:
-            
+
             time.sleep(1)
             if mqconn.is_connected():
                 mqconn.disconnect()
-
