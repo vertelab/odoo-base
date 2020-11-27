2f6fb1467882320a781381a6f6b5a06de127fa3a
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2020-11-27 12:31:09 +0530

    [ADD] partner_legacy_id: AFC-1382c Added partner_legacy_id module.

---
+++ partner_legacy_id/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Legacy Partner ID',
+    'version': '12.0.1.0.0',
+    'author': 'Vertel AB',
+    'website': 'http://www.vertel.se',
+    'license': 'AGPL-3',
+    'depends': [
+    ],
+    'data': [
+        'views/partner_view.xml',
+    ],
+    'installable': True,
+}
