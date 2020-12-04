65b44ea36c097245bf29325d60a99f221e9a72ff
Yevhen Pechurin <yevhen.pechurin@n-development.com>
2020-12-04 22:21:18 +0200

    [ADD] partner_tlr_update AFC-1382b: added module partner_tlr_update

---
+++ partner_tlr_update/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+
+################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2019 N-Development (<https://n-development.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+{
+    'name': 'Partner TLR Update',
+    'version': '12.0.0.0.1',
+    'category': 'Tools',
+    'description': """This module links the TLR-API to the Odoo-logic and 
+    adds res-partner-data for the company and its contac-persons.""",
+
+    'author': "N-development",
+    'license': 'AGPL-3',
+    'website': 'https://www.n-development.com',
+    "depends": [
+        'api_ipf_tlr_client',
+        'partner_legacy_id',
+        'partner_firstname',
+        'hr_departments_partner',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'images': [
+        'static/description/img.png'
+    ],
+}
