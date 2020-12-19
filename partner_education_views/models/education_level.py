d8dcf1ff8383493569beedc93c9301e33c945d00
Anders Wallenquist <waland@fossa.vertel.se>
2020-12-19 10:21:25 +0000

    requirements.txt: new file other: new _descriptiopns

--- partner_education_views/models/education_level.py
+++ partner_education_views/models/education_level.py
@@ -3,6 +3,7 @@ from odoo import models, fields
 
 class ResPartnerEducationLevel(models.Model):
     _name = "res.partner.education_level"
+    _description = """Value store for education level."""
 
     name = fields.Integer(string="Education level")
     partner_ids = fields.One2many(comodel_name="res.partner",
