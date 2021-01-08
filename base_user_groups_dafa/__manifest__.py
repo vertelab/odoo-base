58ef4986391e3b09bc4fca89d352ec721ee7abec
Artur Chilangwa <artur.chilangwa@vertel.se>
2021-01-08 10:21:56 +0100

    add triple quoutes to get rid of EOL error

--- base_user_groups_dafa/__manifest__.py
+++ base_user_groups_dafa/__manifest__.py
@@ -4,8 +4,8 @@
     "version": "12.0.1.0.2",
     "author": "Arbetsformedlingen",
     "license": "AGPL-3",
-    "description": "User groups for the DAFA server. Access rules are added in each module.\n
-    AFC-1590 Added Supportgroups for 1st and 2nd line. Fixed version numbers",
+    "description": """User groups for the DAFA server. Access rules are added in each module.\n
+    AFC-1590 Added Supportgroups for 1st and 2nd line. Fixed version numbers""",
     "website": "https://arbetsformedlingen.se/",
     "category": "Security",
     "depends": [
