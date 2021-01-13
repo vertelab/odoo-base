e7d596a3fa1d92d1937c12fb90488a87fea9869d
Sebastian Gieser <sebastian.gieser@vertel.se>
2021-01-13 16:56:59 +0100

    partner_education_views AFC-1566: removed res.partner.education from res_partner.py

--- partner_education_views/models/res_partner.py
+++ partner_education_views/models/res_partner.py
@@ -20,14 +20,4 @@ class ResPartner(models.Model):
     def _compute_has_drivers_license(self):
         self.har_drivers_license = len(self.drivers_license_ids) > 0
 
-class ResPartnerEducation(models.Model):
-    _name = "res.partner.education"
 
-    sun_id = fields.Many2one(comodel_name='res.sun',
-                               string='SUN Code')
-    education_level_id = fields.Many2one(
-        comodel_name="res.partner.education_level",
-        string="Education level")
-    foreign_education = fields.Boolean(string="Foreign education")
-    foreign_education_approved = fields.Boolean(
-        string="Foreign education approved")
