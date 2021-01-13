db4efc9808323035e8fa6fda42335bdd3b134409
Sebastian Gieser <sebastian.gieser@vertel.se>
2021-01-13 17:07:51 +0100

    partner_education_views AFC-1566: added partner_ids to education, made view better

--- partner_education_views/models/education.py
+++ partner_education_views/models/education.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class ResPartnerEducation(models.Model):
     _name = "res.partner.education"
 
+    partner_ids = fields.Many2many(comodel_name="res.partner")
     sun_id = fields.Many2one(comodel_name='res.sun',
                                string='SUN Code')
     education_level_id = fields.Many2one(
