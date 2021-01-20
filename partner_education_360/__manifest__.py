f746cb0f458895f09bc0daa1b620433f3d1696fe
Sebastian Gieser <sebastian.gieser@vertel.se>
2021-01-20 15:18:48 +0100

    partner_education_360: new module to add education fields to jobseeker view

---
+++ partner_education_360/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution, third party addon
+#    Copyright (C) 2004-2019 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Partner Education 360',
+    'version': '12.0.1.1',
+    'category': '',
+    'description': """
+Adds education to jobseeker view
+================================================================================================
+
+""",
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': [
+        'partner_view_360',
+        'partner_education'
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+        ],
+    'application': False,
+    'installable': True,
+}
