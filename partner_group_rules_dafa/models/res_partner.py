a4ed6d26527dfb6b2b77ca7ece753bfe47b25c88
Sebastian Gieser <sebastian.gieser@vertel.se>
2021-01-21 11:54:37 +0100

    partner_group_rules_dafa AFC-1672: added check for logged in user group belonging

--- partner_group_rules_dafa/models/res_partner.py
+++ partner_group_rules_dafa/models/res_partner.py
@@ -7,9 +7,9 @@ class ResPartner(models.Model):
     
     @api.multi
     def _compute_show_for_support(self):
-        #check if logged in user does not have group base_user_groups_dafa.1_line_support
-        for partner in self:
-            if partner.is_jobseeker or partner.user_id or partner.employee_id:
-                partner.show_for_support = False
+        if not self.env.user.has_group('base_user_groups_dafa.1_line_support'):
+            for partner in self:
+                if partner.is_jobseeker or partner.user_id or partner.employee_id:
+                    partner.show_for_support = False
     
 
