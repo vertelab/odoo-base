ef36d479b443c6c292cfbd7c7f4e04d78a79f4fa
Fredrik Arvas <fredrik.arvas@vertel.se>
2021-01-26 16:20:49 +0100

    added partner_flip_firstname

---
+++ partner_flip_firstname/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Partner flip firstname',
+    'version': '12.0.0.1.0',						
+    'category': '',
+    'description': """
+Flips the position of the firstname and lastname fields
+===========================================================
+""",
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': [
+        'partner_firstname', 
+    ],
+    'data': [
+			'views/res_partner_view.xml',
+        ],
+    'application': False,
+    'installable': True,
+}
