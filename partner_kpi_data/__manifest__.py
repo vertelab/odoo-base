c400848255247d428d42dc6a796e755980244bcc
Fredrik Arvas <fredrik@arvas.se>
2021-02-16 15:12:30 +0100

    Fixed Intendation

--- partner_kpi_data/__manifest__.py
+++ partner_kpi_data/__manifest__.py
@@ -3,26 +3,22 @@
     'version': '12.0.1.1.3',						
     'category': '',
     'description': """
-KPI \n
-===========================================================\n
-AFC-84\n
-AFC-1788 - added new demo-data and fixed invisible tab for individuals\n
-""",
+    KPI \n
+    ===========================================================\n
+    AFC-84\n
+    AFC-1788 - added new demo-data and fixed invisible tab for individuals\n
+    """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
     'website': 'http://www.vertel.se',
     'depends': [
         'base', 
-        'partner_view_360'
-    ],
+        'partner_view_360'],
     'data': [
-	'views/res_partner_view.xml',
-        'security/ir.model.access.csv'
-        ],
+        'views/res_partner_view.xml',
+        'security/ir.model.access.csv'],
     'demo': [
-	'data/res.partner.kpi.csv'
-        ],
-
+        'data/res.partner.kpi.csv'],
     'application': False,
     'installable': True,
 }
