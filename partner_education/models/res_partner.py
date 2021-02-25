8f7281d6755fff72274d0284ad2e56b8dfce049c
Sebastian Gieser <sebastian.gieser@vertel.se>
2021-02-25 17:09:19 +0100

    partner_education AFC-1753: pep8'd code

--- partner_education/models/res_partner.py
+++ partner_education/models/res_partner.py
@@ -4,15 +4,17 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    education_ids = fields.One2many(string="Educations", comodel_name="res.partner.education", inverse_name="partner_id")
+    education_ids = fields.One2many(string="Educations",
+                                    comodel_name="res.partner.education",
+                                    inverse_name="partner_id")
     cv = fields.Binary('CV')
     cv_file_name = fields.Char()
     references = fields.Binary()
     references_file_name = fields.Char()
     has_drivers_license = fields.Boolean(string="Has drivers license",
-                                         compute='_compute_has_drivers_license')
+                                         compute='_compute_has_drivers_license') # noqa 501
     drivers_license_ids = fields.Many2many(comodel_name='res.drivers_license',
-                                          string='Drivers license class')
+                                           string='Drivers license class')
     has_car = fields.Boolean(string="Has access to car")
 
     @api.one
