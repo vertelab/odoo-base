406284a4118763181f3e9554ae96a461dfbe4dd5
Robin Chatfield <robin.chatfield@vertel.se>
2021-04-08 15:58:48 +0200

    AFC-1747 mail_tracking_x2m: Module created.

---
+++ mail_tracking_x2m/__manifest__.py
@@ -0,0 +1,34 @@
+##############################################################################
+#
+#    Odoo, Open Source Management Solution, third party addon
+#    Copyright (C) 2021 Vertel AB (<http://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Mail Tracking x2many',
+    'version': '12.0.1.0.0',
+    'category': 'Technical Settings',
+    'description': """Enables the track_visibility feature on many2many and one2many fields.""",
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': ['mail'],
+    'data': [],
+    'application': False,
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
