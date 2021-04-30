7d2d6d121e87ced77aa8ea04cea2b92412f0d178
Daniel Eriksson <daer@daer.io>
2021-04-30 10:42:10 +0200

    partner_daily_notes_edi AFC-2137: misc

--- partner_daily_notes_edi/models/res_partner.py
+++ partner_daily_notes_edi/models/res_partner.py
@@ -5,28 +5,29 @@ class ResPartnerNotes(models.Model):
     _inherit = 'res.partner.notes'
 
     route_id = fields.Many2one(comodel_name="edi.route")
-    
+
     @api.multi
     def _edi_message_create(self):
         for note in self:
-            route = self.env['edi.route'].sudo().search([('id', '=', self.env['ir.model.data'].xmlid_to_res_id('edi_af_as_notes.asok_note_post_route'))])
+            route = self.env.ref('edi_af_as_notes.asok_note_post_route')
             vals = {
                 'name': 'set contact msg',
                 'edi_type': self.env.ref('edi_af_as_notes.edi_af_as_notes_post').id,
                 'model': note._name,
                 'res_id': note.id,
-                'route_id': self.env.ref('edi_af_as_notes.asok_note_post_route').id,
+                'route_id': route.id,
                 'route_type': 'edi_af_as_notes_post',
             }
             message = self.env['edi.message'].create(vals)
             message.pack()
             route.run()
+            # TODO: add some kind of feedback to user that this failed?
 
     @api.model
     def create(self, values):
         """
         Trigger new notes and creates edi-messages for them
         """
-        rec = super(ResPartnerNotes,self).create(values)
+        rec = super(ResPartnerNotes, self).create(values)
         rec._edi_message_create()
         return rec
