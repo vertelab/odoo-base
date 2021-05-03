e1de6c1cdf65b597979536d416be8877cbce6f6b
Robin Chatfield <robin.chatfield@arbetsformedlingen.se>
2021-05-03 16:05:00 +0200

    FC-1985 web_extra_options: Removed test function.

--- web_extra_options/__init__.py
+++ web_extra_options/__init__.py
@@ -2,10 +2,3 @@ from odoo import models, api
 
 import logging
 _logger = logging.getLogger(__name__)
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    @api.model
-    def test_log_stuff(self):
-        return _logger
