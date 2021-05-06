467430edbc8ac53d89bf590187a315472b37ba7c
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-05-06 12:39:17 +0530

    [ADD] partner_design_mocukup: AFC-2170 Added module partner_design_mocukup to search partner with name and email.

---
+++ partner_design_mocukup/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Partner Design Mockup',
+    'version': '12.0.0.1',
+    'category': '',
+    'description': """
+    V12.0.0.1 AFC-2170 - This module adds two different ways of displaying Odoo content. A normal formview with tabs and a custom view with different boxes with content-views. \n
+""",
+    'author': 'Vertel AB',
+    'license': 'AGPL-3',
+    'website': 'http://www.vertel.se',
+    'depends': [
+        'crm', 'project'
+    ],
+    'data': [
+        'wizard/search_partners_views.xml',
+        'views/res_partner_views.xml'
+    ],
+    'application': False,
+    'installable': True,
+}
