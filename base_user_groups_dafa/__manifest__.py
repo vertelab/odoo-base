340422bec42a357630baccea423adadba565fbed
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-05-17 15:17:51 +0200

    AFC-2117 added group for translation accountants

--- base_user_groups_dafa/__manifest__.py
+++ base_user_groups_dafa/__manifest__.py
@@ -1,12 +1,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "DAFA User Groups",
-    "version": "12.0.1.0.3",
+    "version": "12.0.1.0.4",
     "author": "Arbetsformedlingen",
     "license": "AGPL-3",
     "description": """User groups for the DAFA server. Access rules are added in each module.\n
     AFC-1590 Added Supportgroups for 1st and 2nd line. Fixed version numbers\n
     AFC-1747 Update in translation.\n
+    AFC-2117 Added New group for translation accountants.\n
     """,
     "website": "https://arbetsformedlingen.se/",
     "category": "Security",
