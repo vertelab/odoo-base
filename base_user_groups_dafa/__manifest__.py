1b737ccd2ccb7bc48d09c5e49e80d0f8dadb262b
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-05-24 10:57:08 +0200

    AFC-2090 update manifest

--- base_user_groups_dafa/__manifest__.py
+++ base_user_groups_dafa/__manifest__.py
@@ -1,13 +1,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "DAFA User Groups",
-    "version": "12.0.1.0.4",
+    "version": "12.0.1.0.5",
     "author": "Arbetsformedlingen",
     "license": "AGPL-3",
     "description": """User groups for the DAFA server. Access rules are added in each module.\n
     AFC-1590 Added Supportgroups for 1st and 2nd line. Fixed version numbers\n
     AFC-1747 Update in translation.\n
     AFC-2117 Added New group for translation accountants.\n
+    AFC-2090 Added SalesRole to AccountingRead-group \n
     """,
     "website": "https://arbetsformedlingen.se/",
     "category": "Security",
