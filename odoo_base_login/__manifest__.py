5d4b37d3026fa0ee2976ce59a48dd8d76c685826
Simon Arnshed <simon.arnshed@arbetsformedlingen.se>
2021-07-01 15:06:00 +0200

    odoo_base_login AFC-2142: Removed "length" from Kanban view

--- odoo_base_login/__manifest__.py
+++ odoo_base_login/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Login with reason',
     'summary':'Audit login users of certain group with logged in reason',
-    'version': '12.0.0.2.1',
+    'version': '12.0.0.2.2',
     'category': 'Tools',
     'description': """
 Base Login with reason
