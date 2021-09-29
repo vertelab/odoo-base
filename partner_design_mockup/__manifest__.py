4e493cd95896a43fe8ccb5e9ae5f56ad5eff8080
Rupareliya Hemangi <rupareliyahemangi145@gmail.com>
2021-09-29 18:08:00 +0530

    [IMP] partner_design_mockup: AFC-2674 Improved code.

--- partner_design_mockup/__manifest__.py
+++ partner_design_mockup/__manifest__.py
@@ -1,16 +1,16 @@
 {
     'name': 'Partner Design Mockup',
-    'version': '12.0.0.6',
+    'version': '12.0.0.1.0',
     'category': '',
     'description': """
     Download 'One Click Form Edit' module from here : \n
     https://apps.odoo.com/apps/modules/12.0/one_click_form_edit/\n
-    V12.0.0.1 AFC-2170 - This module adds two different ways of displaying Odoo content. A normal formview with tabs and a custom view with different boxes with content-views. \n
-    V12.0.0.2 AFC-2170 - Added search box in second type of view to search partners. \n
-    V12.0.0.3 AFC-2170 - Added Advance search button in second type of view to search partners. \n
-    V12.0.0.3 AFC-2170 - Added Advance search button in second type of view to search partners with separate menu. \n
-    V12.0.0.5 AFC-2674 - Added Contact links section inside mocukup form views.\n
-    V12.0.0.6 AFC-2674 - Added dropdown and search button at top.\n
+    V12.0.0.0.1 AFC-2170 - This module adds two different ways of displaying Odoo content. A normal formview with tabs and a custom view with different boxes with content-views. \n
+    V12.0.0.0.2 AFC-2170 - Added search box in second type of view to search partners. \n
+    V12.0.0.0.3 AFC-2170 - Added Advance search button in second type of view to search partners. \n
+    V12.0.0.0.4 AFC-2170 - Added Advance search button in second type of view to search partners with separate menu. \n
+    V12.0.0.0.5 AFC-2674 - Added Contact links section inside mocukup form views.\n
+    V12.0.0.1.0 AFC-2674 - Added dropdown and search button at top.\n
 """,
     'author': 'Vertel AB',
     'license': 'AGPL-3',
