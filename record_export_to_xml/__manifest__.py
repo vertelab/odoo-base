87f2fb9d9b48bc158d9a0d34be5723efb732844e
Miracle Ayodele <miracleaayodele@gmail.com>
2021-10-04 12:37:22 +0100

    added documentation for the module

--- record_export_to_xml/__manifest__.py
+++ record_export_to_xml/__manifest__.py
@@ -4,7 +4,10 @@
     'version': '14.0.0.1.0',
     'category': 'Extra Tools',
     'summary': 'Export Records to XML',
-    'description': """Export Records to XML""",
+    'description': """
+        Export Records to XML
+        14.0.0.1.1 - Added Documentations
+    """,
     'author': 'Vertelab',
     'website': "https://www.vertel.se",
     'depends': ['base', 'event', 'hr', 'sale', 'project'],
