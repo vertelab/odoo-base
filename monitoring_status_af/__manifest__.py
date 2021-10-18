a3c442d7022692d76cf6711be8e95cafab24fafc
Fredrik Arvas, Vertel AB <61729317+fredrikarvas-vertel@users.noreply.github.com>
2021-10-18 13:59:08 +0200

    Update __manifest__.py

--- monitoring_status_af/__manifest__.py
+++ monitoring_status_af/__manifest__.py
@@ -2,9 +2,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
     'name': 'Monitoring: Status Af',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'description': """
         12.0.1.0.1 - AFC-1851 Taken Monitoring value from version.txt instead of static.\n
+        12.0.1.1.0  Added link to where the module is maintained. \n
+        This module is maintained from: https://github.com/vertelab/odoo-base/tree/Dev-12.0
     """,
     'author': "Camptocamp,Odoo Community Association (OCA)','Vertel AB'",
     'license': 'AGPL-3',
