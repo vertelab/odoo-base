116cd127657462bb0440d567fbfaba45506c1a9c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 15:52:03 +0200

    Update __manifest__.py

--- partner_gender/__manifest__.py
+++ partner_gender/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -14,25 +14,29 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Affero General Public License for more details.
 #
-
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
     'name': 'Partner Gender',
-    'version': '14.0.0.1',
+    'summary': 'Adds a "Personal information" tab on res_partners with a gender field.',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-event.git',
     'category': 'Customer Relationship Management',
-    'summary': 'Adds a "Personal information" tab on res_partners with a gender field',
+    'version': '14.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/',
     'description': """
         Features:\n
         * Adds a "Personal information" tab on res_partners with a gender field\n
         * Adds an select box for gender on personal details form in the user portal\n
         This module is maintained from: https://github.com/vertelab/odoo-base/tree/14.0/partner_gender/\n
     """,
-    'author': 'Vertel AB',
-    'website': 'https://www.vertel.se',
     'depends': ['base'],
     'data': [
         'views/res_partner.xml',
