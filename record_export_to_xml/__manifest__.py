036cd8a8e8ae4fc9e4069ed73a87b4788253a47d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 15:15:16 +0200

    Update __manifest__.py

--- record_export_to_xml/__manifest__.py
+++ record_export_to_xml/__manifest__.py
@@ -1,9 +1,36 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
     'name': 'Export Records to XML',
-    'version': '14.0.0.1.1',
-    'category': 'Extra Tools',
     'summary': 'Export Records to XML',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-event.git',
+    'category': 'Extra Tools',
+    'version': '14.0.0.1.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/',
     'description': """
         Export Records to XML - Models you can import include: \n
             - users
@@ -14,8 +41,6 @@
             - project/tasks
         \n 14.0.0.1.1 - Added Documentations
     """,
-    'author': 'Vertelab',
-    'website': "https://www.vertel.se",
     'depends': ['base', 'event', 'hr', 'sale', 'project'],
     'data': [
         "security/ir.model.access.csv",
