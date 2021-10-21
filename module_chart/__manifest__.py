3db99c98f39ec1d569694cbf087a787c6e628e67
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 15:49:12 +0200

    Update __manifest__.py

--- module_chart/__manifest__.py
+++ module_chart/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ################################################################################
 #
-#    OpenERP, Open Source Management Solution
+#    Odoo SA, Open Source Management Solution
 #    Copyright (C) 2019 N-Development (<https://n-development.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -21,17 +21,20 @@
 
 {
     'name': 'Base Module depends Chart',
-    'version': '14.0.1.0.0',
+        'summary': '...',
+    'author': 'Vertel AB',
+	'contributor': 'Vertel AB, N-Development',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-event.git',
     'category': 'Tools',
+    'version': '14.0.1.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
+    'website': 'https://vertel.se/',
     'description': """This module adds a graphical description of the Odoo database dependencys.\n
 	Install it and go to Apps -> Module Graph to see a dynamic map of the installed modules.\n-development\n
 	This module is maintained from: https://github.com/vertelab/odoo-base/ \n
 	""",
-    'author': "Vertel AB",
-    'license': 'AGPL-3',
-    'website': 'https://www.vertel.ab',
-	'contributors': 'Vertel AB, N-Development',
-	'maintainers': 'Vertel AB',
     'depends': [
         'base',
         'web',
