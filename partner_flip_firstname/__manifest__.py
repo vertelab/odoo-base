c201c09200dffa76a69318a7d65b6d46f4810275
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-01-10 13:48:33 +0000

    Added flip for contact wizard

--- partner_flip_firstname/__manifest__.py
+++ partner_flip_firstname/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Partner flip firstname',
-    'version': '14.0.0.1.0',
+    'version': '14.0.0.2.0',
     'category': '',
     'description': """
         Flips the position of the firstname and lastname fields on users and partners
