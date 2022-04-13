8c228b43024651ba5afa5b8786f702dc821d2dcd
Daniel Löfgren <dan.lofgrens@gmail.com>
2022-04-13 08:07:38 +0000

    Check bad stuff?

--- partner_gender/controllers/main.py
+++ partner_gender/controllers/main.py
@@ -2,111 +2,112 @@ from odoo import fields as odoo_fields, http, tools, _, SUPERUSER_ID
 from odoo.http import request, route
 from odoo.exceptions import ValidationError, AccessError, MissingError, UserError, AccessDenied
 from odoo.addons.portal.controllers.portal import CustomerPortal
+from requests import post
 
 
 class CustomerPortalInherit(CustomerPortal):
-    # MANDATORY_BILLING_FIELDS = [
-    #     "name", "phone", "email", "street", "city", "country_id", "gender"]
-    # OPTIONAL_BILLING_FIELDS = [
-    #     "zipcode", "state_id", "vat", "company_name", "gender_txt"]
-
-    # def __init__(self) -> None:
-    #     super(CustomerPortalInherit, self).__init__()
-
-    SKS_MANDATORY_BILLING_FIELDS = [
+    MANDATORY_BILLING_FIELDS = [
         "name", "phone", "email", "street", "city", "country_id", "gender"]
-    SKS_OPTIONAL_BILLING_FIELDS = [
+    OPTIONAL_BILLING_FIELDS = [
         "zipcode", "state_id", "vat", "company_name", "gender_txt"]
 
-    @route(['/my/account'], type='http', auth='user', website=True)
-    def account(self, redirect=None, **post):
-        values = self._prepare_portal_layout_values()
-        partner = request.env.user.partner_id
-        values.update({
-            'error': {},
-            'error_message': [],
-        })
+    def __new__(cls):
+        cls.MANDATORY_BILLING_FIELDS = ["name", "phone", "email", "street", "city", "country_id", "gender"]
 
-        if post and request.httprequest.method == 'POST':
-            error, error_message = self.details_form_validate(post)
-            values.update({'error': error, 'error_message': error_message})
-            values.update(post)
-            if not error:
-                values = {key: post[key]
-                          for key in self.SKS_MANDATORY_BILLING_FIELDS}
-                values.update(
-                    {key: post[key] for key in self.SKS_OPTIONAL_BILLING_FIELDS if key in post})
-                for field in set(['country_id', 'state_id']) & set(values.keys()):
-                    try:
-                        values[field] = int(values[field])
-                    except:
-                        values[field] = False
-                values.update({'zip': values.pop('zipcode', '')})
-                partner.sudo().write(values)
-                if redirect:
-                    return request.redirect(redirect)
-                return request.redirect('/my/home')
+    # SKS_MANDATORY_BILLING_FIELDS = [
+    #     "name", "phone", "email", "street", "city", "country_id", "gender"]
+    # SKS_OPTIONAL_BILLING_FIELDS = [
+    #     "zipcode", "state_id", "vat", "company_name", "gender_txt"]
 
-        countries = request.env['res.country'].sudo().search([])
-        states = request.env['res.country.state'].sudo().search([])
+    # @route(['/my/account'], type='http', auth='user', website=True)
+    # def account(self, redirect=None, **post):
+    #     values = self._prepare_portal_layout_values()
+    #     partner = request.env.user.partner_id
+    #     values.update({
+    #         'error': {},
+    #         'error_message': [],
+    #     })
 
-        values.update({
-            'partner': partner,
-            'countries': countries,
-            'states': states,
-            'has_check_vat': hasattr(request.env['res.partner'], 'check_vat'),
-            'redirect': redirect,
-            'page_name': 'my_details',
-        })
+    #     if post and request.httprequest.method == 'POST':
+    #         error, error_message = self.details_form_validate(post)
+    #         values.update({'error': error, 'error_message': error_message})
+    #         values.update(post)
+    #         if not error:
+    #             values = {key: post[key]
+    #                       for key in self.SKS_MANDATORY_BILLING_FIELDS}
+    #             values.update(
+    #                 {key: post[key] for key in self.SKS_OPTIONAL_BILLING_FIELDS if key in post})
+    #             for field in set(['country_id', 'state_id']) & set(values.keys()):
+    #                 try:
+    #                     values[field] = int(values[field])
+    #                 except:
+    #                     values[field] = False
+    #             values.update({'zip': values.pop('zipcode', '')})
+    #             partner.sudo().write(values)
+    #             if redirect:
+    #                 return request.redirect(redirect)
+    #             return request.redirect('/my/home')
 
-        response = request.render("portal.portal_my_details", values)
-        response.headers['X-Frame-Options'] = 'DENY'
-        return response
+    #     countries = request.env['res.country'].sudo().search([])
+    #     states = request.env['res.country.state'].sudo().search([])
 
-    def details_form_validate(self, data):
-        error = dict()
-        error_message = []
+    #     values.update({
+    #         'partner': partner,
+    #         'countries': countries,
+    #         'states': states,
+    #         'has_check_vat': hasattr(request.env['res.partner'], 'check_vat'),
+    #         'redirect': redirect,
+    #         'page_name': 'my_details',
+    #     })
 
-        # Validation
-        for field_name in self.SKS_MANDATORY_BILLING_FIELDS:
-            if not data.get(field_name):
-                error[field_name] = 'missing'
+    #     response = request.render("portal.portal_my_details", values)
+    #     response.headers['X-Frame-Options'] = 'DENY'
+    #     return response
 
-        # email validation
-        if data.get('email') and not tools.single_email_re.match(data.get('email')):
-            error["email"] = 'error'
-            error_message.append(
-                _('Invalid Email! Please enter a valid email address.'))
+    # def details_form_validate(self, data):
+    #     error = dict()
+    #     error_message = []
 
-        # vat validation
-        partner = request.env.user.partner_id
-        if data.get("vat") and partner and partner.vat != data.get("vat"):
-            if partner.can_edit_vat():
-                if hasattr(partner, "check_vat"):
-                    if data.get("country_id"):
-                        data["vat"] = request.env["res.partner"].fix_eu_vat_number(
-                            int(data.get("country_id")), data.get("vat"))
-                    partner_dummy = partner.new({
-                        'vat': data['vat'],
-                        'country_id': (int(data['country_id'])
-                                       if data.get('country_id') else False),
-                    })
-                    try:
-                        partner_dummy.check_vat()
-                    except ValidationError:
-                        error["vat"] = 'error'
-            else:
-                error_message.append(
-                    _('Changing VAT number is not allowed once document(s) have been issued for your account. Please contact us directly for this operation.'))
+    #     # Validation
+    #     for field_name in self.SKS_MANDATORY_BILLING_FIELDS:
+    #         if not data.get(field_name):
+    #             error[field_name] = 'missing'
 
-        # error message for empty required fields
-        if [err for err in error.values() if err == 'missing']:
-            error_message.append(_('Some required fields are empty.'))
+    #     # email validation
+    #     if data.get('email') and not tools.single_email_re.match(data.get('email')):
+    #         error["email"] = 'error'
+    #         error_message.append(
+    #             _('Invalid Email! Please enter a valid email address.'))
 
-        unknown = [k for k in data if k not in self.SKS_MANDATORY_BILLING_FIELDS
-                   + self.SKS_OPTIONAL_BILLING_FIELDS]
-        if unknown:
-            error['common'] = 'Unknown field'
-            error_message.append("Unknown field '%s'" % ','.join(unknown))
+    #     # vat validation
+    #     partner = request.env.user.partner_id
+    #     if data.get("vat") and partner and partner.vat != data.get("vat"):
+    #         if partner.can_edit_vat():
+    #             if hasattr(partner, "check_vat"):
+    #                 if data.get("country_id"):
+    #                     data["vat"] = request.env["res.partner"].fix_eu_vat_number(
+    #                         int(data.get("country_id")), data.get("vat"))
+    #                 partner_dummy = partner.new({
+    #                     'vat': data['vat'],
+    #                     'country_id': (int(data['country_id'])
+    #                                    if data.get('country_id') else False),
+    #                 })
+    #                 try:
+    #                     partner_dummy.check_vat()
+    #                 except ValidationError:
+    #                     error["vat"] = 'error'
+    #         else:
+    #             error_message.append(
+    #                 _('Changing VAT number is not allowed once document(s) have been issued for your account. Please contact us directly for this operation.'))
 
-        return error, error_message
+    #     # error message for empty required fields
+    #     if [err for err in error.values() if err == 'missing']:
+    #         error_message.append(_('Some required fields are empty.'))
+
+    #     unknown = [k for k in data if k not in self.SKS_MANDATORY_BILLING_FIELDS
+    #                + self.SKS_OPTIONAL_BILLING_FIELDS]
+    #     if unknown:
+    #         error['common'] = 'Unknown field'
+    #         error_message.append("Unknown field '%s'" % ','.join(unknown))
+
+    #     return error, error_message
