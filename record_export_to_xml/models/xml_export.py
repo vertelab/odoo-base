add255504c86c69954df467d5e78ada4ee5063d0
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-04-21 11:15:26 +0000

    add names also

--- record_export_to_xml/models/xml_export.py
+++ record_export_to_xml/models/xml_export.py
@@ -7,7 +7,7 @@ from fnmatch import fnmatch
 class XMLExport(models.TransientModel):
     _name = 'xml.export'
 
-    def generate_xml_record(self, model_records, records, ir_model_id):
+    def generate_xml_record(self, model_records, records, ir_model_id, names):
         record = ET.SubElement(records, "record")
         excluded_fields = ['create_date', 'message_ids', 'id', 'write_date', 'create_uid', '__last_update',
                            'write_uid', 'active', 'my_activity_date_deadline', 'resource_id',
@@ -41,6 +41,7 @@ class XMLExport(models.TransientModel):
                             mtomvalues.append("(4, ref('%s'))" % list(values)[0][1])
                     if len(mtomvalues) > 0:
                         ET.SubElement(record, 'field', name=field.name, eval="[%s]" % (','.join(mtomvalues)))
+        return record
 
     def generate_xml(self):
         ir_model_id = self.env['ir.model'].search([('model', '=', self._context['active_model'])])
@@ -55,7 +56,7 @@ class XMLExport(models.TransientModel):
         names = [name for name in model_id.fields_get().keys() if fnmatch(name, 'in_group*')] + \
                 [name for name in model_id.fields_get().keys() if fnmatch(name, 'sel_groups*')]
         for model_records in model_id:
-            self.generate_xml_record(model_records, records, ir_model_id)
+            self.generate_xml_record(model_records, records, ir_model_id, names)
 
         return xml
 
