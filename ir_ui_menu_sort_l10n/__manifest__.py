744e7c7332f08ba8bea23161a9aa8d1a50d48896
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-06-14 15:47:12 +0200

    Update __manifest__.py
    fixing with manifest

--- ir_ui_menu_sort_l10n/__manifest__.py
+++ ir_ui_menu_sort_l10n/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,20 +20,20 @@
 ##############################################################################
 
 {
-    'name': 'Menu Sorting',
+    'name': 'Base: Menu Sort',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Sort menus by name',
+    'category': 'Technical',
+    'description': 'Sort menus by name',
+    #'sequence': '1',
     'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-base/ir_ui_menu_sort_l10n',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
     'contributor': '',
     'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-base',
-    'category': '',
-    'version': '14.0.1.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/apps/base',
-    "description": """
-        Sort menus by name
-    """,
+    'repository': 'https://github.com/vertelab/odoo-base.git',
     "depends": ["base"],
     "data": [
     ],
