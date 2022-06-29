257288765dad4434e364cda44626b1b839d44015
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-06-29 14:48:00 +0200

    Update __manifest__.py

--- partner_flip_zip_city/__manifest__.py
+++ partner_flip_zip_city/__manifest__.py
@@ -32,7 +32,7 @@
     #'sequence': '1',
     'author': 'Vertel AB',
     'website': 'https://vertel.se/apps/odoo-base/partner_flip_zip_city',
-    'images': ['/static/description/banner.png'], # 560x280 px.
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
     'maintainer': 'Vertel AB',
