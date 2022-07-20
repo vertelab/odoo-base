91d67006efa29e98d6794466b3fcda09796294ef
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-07-20 12:32:11 +0000

    partner_customer_sequence: Added functionality for it to be set on already existing customers

--- partner_customer_sequence/models/res_partner.py
+++ partner_customer_sequence/models/res_partner.py
@@ -1,10 +1,25 @@
 from odoo import models, fields, api, _
+import logging
+_logger = logging.getLogger(__name__)
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+    
+    @api.model
+    def set_all_customers_number(self):
+        _logger.warning("set_all_customers_number"*100)
+        partners = self.env['res.partner'].search([('customer_rank','>',0)])
+        _logger.warning(f"{partners=}")
+        partners._set_costumer_number()
+    
+    def _set_costumer_number(self):
+        for rec in self:
+            if rec.customer_rank > 0 and not rec.customer_sequence:
+               rec.customer_sequence = self.env['ir.sequence'].next_by_code('res.partner')
 
-    customer_sequence = fields.Char(string='Sequence', readonly=True)
+    customer_sequence = fields.Char(string='Customer Number', readonly=True)
 
+    
     @api.model
     def create(self, values):
         res = super(ResPartner, self).create(values)        
@@ -19,3 +34,8 @@ class ResPartner(models.Model):
         if self.customer_rank and not self.customer_sequence:
             self.customer_sequence = self.env['ir.sequence'].next_by_code('res.partner')
         return res
+        
+        # ~ def _default_description(self):
+            # ~ return self.env['ir.ui.view']._render_template('event.event_default_descripton')
+
+        # ~ description = fields.Html(string='Description', translate=html_translate, sanitize_attributes=False, sanitize_form=False, default=_default_description)
