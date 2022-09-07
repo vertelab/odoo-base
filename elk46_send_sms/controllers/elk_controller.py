46fbc4d20f44b1b2fd049363f4d77b07015a6e30
Dmitri Iselund <Dmitri.iselund@gmail.com>
2022-09-07 13:36:17 +0000

    tested controller, handles both failed and delivereded messages now

--- elk46_send_sms/controllers/elk_controller.py
+++ elk46_send_sms/controllers/elk_controller.py
@@ -1,5 +1,6 @@
+from urllib import response
 import odoo.http as http
-from odoo.http import request
+from odoo.http import request, Response
 import logging
 
 _logger = logging.getLogger("------dmitri-------")
@@ -14,5 +15,22 @@ class ElkController(http.Controller):
 
         TODO: Enter name of service and link to doc.
         '''
-        _logger.warning(f"hello {kwargs=}")
-        return {'response': 'OK'}
+        _logger.warning(f"incoming POST from 46elks {kwargs=}")
+        if kwargs:
+            if kwargs['status'] != 'created' and kwargs['status'] != 'sent':
+                try:
+                    saved_sms_id = http.request.env['temp.elk.sms'].sudo().search([('elk_api_id', '=', kwargs['id'])])
+                    saved_sms_record = saved_sms_id
+                    saved_sms_record.status = kwargs['status']
+
+                    sale_order = http.request.env['sale.order'].sudo().browse(saved_sms_record.sale_id)
+                    if kwargs['status'] == 'delivered':
+                        sale_order.message_post(body = f'Message with body: {saved_sms_record.body}, to number: {saved_sms_record.number}, was sucessfully sent.')
+                    else:
+                        sale_order.message_post(body = f'Message with body: {saved_sms_record.body}, to number: {saved_sms_record.number}, failed to send.')
+                except Exception as e:
+
+                     _logger.warning(e)
+
+        return Response(status=200)
+
--- elk46_send_sms/models/elk_sms.py
+++ elk46_send_sms/models/elk_sms.py
@@ -17,32 +17,39 @@ class ElkSms(models.Model):
     #46739299019
     #auth=('a6ba22729971395309fd3a973ee352f93', 'AFE26B724029E6B79CFFAAD9C9762545')
     def send(self, number=False, body=False, delete_all=False, auto_commit=False, raise_exception=False):
+       
         try:
             auth_info = (self.env['ir.config_parameter'].get_param('elk_sms_auth')).split(',')
         except:
             raise UserError(_('Error. Create a system parameter called "elk_sms_auth" containing the auth info like this: username,password'))
 
-        def read_json_from_site(response):
-            url = 'https://api.46elks.com/a1/sms'
-            request = urllib.request.Request(url)
-            string = '%s:%s' % (auth_info[0], auth_info[1])
-            base64string = base64.standard_b64encode(string.encode('utf-8'))
-            request.add_header('Authorization', 'Basic %s' % base64string.decode('utf-8'))
-            result = urllib.request.urlopen(request)
-            result_json = json.loads(result.read())
+        try:
+            dryrun_toggle = (self.env['ir.config_parameter'].get_param('elk_sms_dryrun')).split(',')
+        except:
+            raise UserError(_('Error. Create a system parameter called "elk_sms_dryrun" containing a yes or no depending on if you want to send actual text messages or not. '))
 
-            for line in result_json['data']:
-                if line['id'] == json.loads(response.content.decode('utf-8'))['id']:
-                    _logger.warning(line['status'])
-                    if line['status'] == 'delivered':
 
-                        return True
-            raise UserError(_('Error. Did you input the correct number?'))
+        # def read_json_from_site(response):
+        #     url = 'https://api.46elks.com/a1/sms'
+        #     request = urllib.request.Request(url)
+        #     string = '%s:%s' % (auth_info[0], auth_info[1])
+        #     base64string = base64.standard_b64encode(string.encode('utf-8'))
+        #     request.add_header('Authorization', 'Basic %s' % base64string.decode('utf-8'))
+        #     result = urllib.request.urlopen(request)
+        #     result_json = json.loads(result.read())
+
+        #     for line in result_json['data']:
+        #         if line['id'] == json.loads(response.content.decode('utf-8'))['id']:
+        #             _logger.warning(line['status'])
+        #             if line['status'] == 'delivered':
+
+        #                 return True
+        #     raise UserError(_('Error. Did you input the correct number?'))
         
         if auth_info:
             response = requests.post('https://api.46elks.com/a1/sms',   auth=(auth_info[0], auth_info[1]),
-                                     data={'dryrun': 'no', 'from': 'Reboot', 'to': self.convert_number(number), 'message': body,
-                                     'whendelivered': 'http://moe.vertel.se:1014/sms?db=reboot'})
+                                     data={'dryrun': dryrun_toggle, 'from': 'Reboot', 'to': self.convert_number(number), 'message': body,
+                                     'whendelivered': 'http://moe.vertel.se:1014/sms'})
             
 
             if 'Unexpected 0' in response.content.decode('utf-8'):
@@ -50,6 +57,14 @@ class ElkSms(models.Model):
 
             if 'requires Basic' in response.content.decode('utf-8'):
                 raise UserError(_('Error. Check if the auth info in the system parameter "elk_sms_auth" is valid.'))
+
+            if 'Not enough credits' in response.content.decode('utf-8'):
+                raise UserError(_('Error. Not enough money to send message, add funds.'))
+
+            if 'Missing key to' in response.content.decode('utf-8'):
+                raise UserError(_('Error. Missing key to (i have no idea to what, probably weird phone number.)'))
+
+
 
 
             return response
