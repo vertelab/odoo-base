92c7754bb797ab425377491d9a90af71fddaa78f
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2022-10-11 08:45:52 +0000

    elk46_send_sms: added more controlls so that we won't try to send empty sms.

--- elk46_send_sms/models/elk_sms.py
+++ elk46_send_sms/models/elk_sms.py
@@ -16,6 +16,14 @@ class ElkSms(models.Model):
     url = fields.Char('https://api.46elks.com/a1/sms')
 
     def send(self, delete_all=False, auto_commit=False, raise_exception=False):
+        if not self.body:
+           _logger.warning(f"Unable to send sms: Missing Message {self=}")
+           self.state  = "error"
+           self.error_code = "sms_number_missing"
+        if not self.number:
+           _logger.warning(f"Unable to send sms: Missing Number {self=}")
+           self.state  = "error"
+           self.error_code = "sms_server" 
 
         try:
             auth_info = (self.env['ir.config_parameter'].get_param('elk_sms_auth')).split(',')
@@ -35,16 +43,21 @@ class ElkSms(models.Model):
             response = requests.post('https://api.46elks.com/a1/sms', auth=(auth_info[0], auth_info[1]),
                                      data={'dryrun': dryrun_toggle, 'from': 'Reboot', 'to': self.convert_number(self.number),'message': self.body,'whendelivered': f"{self.env['ir.config_parameter'].get_param('web.base.url')}/sms"})
             if 'Unexpected 0' in response.content.decode('utf-8'):
-                raise UserError(_('Error. Input a +46.... number instead of 0...'))
-
+                #raise UserError(_('Error. Input a +46.... number instead of 0...'))
+                _logger.warning(_('Error. Input a +46.... number instead of 0...'))
+                self.state  = "error"
             if 'requires Basic' in response.content.decode('utf-8'):
-                raise UserError(_('Error. Check if the auth info in the system parameter "elk_sms_auth" is valid.'))
-
+                #raise UserError(_('Error. Check if the auth info in the system parameter "elk_sms_auth" is valid.'))
+                _logger.warning(_('Error. Check if the auth info in the system parameter "elk_sms_auth" is valid.'))
+                self.state  = "error"
             if 'Not enough credits' in response.content.decode('utf-8'):
-                raise UserError(_('Error. Not enough money to send message, add funds.'))
-
+                #raise UserError(_('Error. Not enough money to send message, add funds.'))
+                _logger.warning(_('Error. Not enough money to send message, add funds.'))
+                self.state  = "error"
             if 'Missing key to' in response.content.decode('utf-8'):
-                raise UserError(_('Error. Missing key to (i have no idea to what, probably weird phone number.)'))
+                #raise UserError(_('Error. Missing key to (i have no idea to what, probably weird phone number.)'))
+                _logger.warning(_('Error. Missing key to (i have no idea to what, probably weird phone number.)'))
+                self.state  = "error"
             self.state  = "sent"
             return response
 
