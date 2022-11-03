5efb71338b64df1aeb75f679705de4cb9512da83
iammiracle <miracleaayodele@gmail.com>
2022-11-03 11:02:44 +0100

    sticky header

---
+++ base_tree_sticky_headers/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Sticky Tree Header",
+    "summary": "Sticky Tree Header",
+    "version": "14.0.1.2.0",
+    "category": "Website",
+    "website": "https://github.com/vertel/odoo-base",
+    "author": "Vertel AB",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["web"],
+    "data": ["views/assets.xml"],
+    "qweb": [
+    ],
+    "sequence": 1,
+}
