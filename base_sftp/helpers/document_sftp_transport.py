c6d579c91f6e35431eac73249c5fb2f0e126a3ee
iammiracle <miracleaayodele@gmail.com>
2023-01-11 16:45:49 +0100

    changes from testing

--- base_sftp/helpers/document_sftp_transport.py
+++ base_sftp/helpers/document_sftp_transport.py
@@ -2,8 +2,8 @@
 # © 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 try:
-    # from paramiko import Transport
-    from paramiko.transport import DEFAULT_WINDOW_SIZE, DEFAULT_MAX_PACKET_SIZE, Transport
+    from paramiko import Transport
+    from paramiko.transport import DEFAULT_WINDOW_SIZE, DEFAULT_MAX_PACKET_SIZE
     from paramiko.common import DEBUG
 except ImportError:
     pass
