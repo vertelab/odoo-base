511a8f7c746208fa6c59a8313e61f77c39e79811
Royne Reinholdsson <royne@outlook.com>
2023-04-27 12:02:13 +0000

    Overwrote the action_create_employee function so that res_partner.employee is set to True when creating an employee.

---
+++ partner_employee_status/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Account: Employee Status',
+    'version': '14.0.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Checks if a res.partner is an employee. Adds a true/false field inside the res.partner.form',
+    'category': 'Accounting',
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': '',
+    'images': ['/static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account',
+    'description': """
+       Checks if a res.partner is an employee. Adds a true/false field inside the res.partner.form
+    """,
+    'depends': ['base','hr'],
+    'data': [
+        'views/res_partner_view.xml',
+        ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
