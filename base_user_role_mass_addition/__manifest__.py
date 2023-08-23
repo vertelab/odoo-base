5b9fd79ae6dbd3fe81c8585025607e9cc9e16f44
Björn Hayer <bjorn.hayer@vertel.se>
2023-08-23 12:58:25 +0000

    Initial commit of base_user_role_mass_addition

---
+++ base_user_role_mass_addition/__manifest__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'User roles: Mass addition',
+    'version': '14.0.0.1.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Extends the User Roles to enable assiging several users to a role, at the same time.',
+    'category': 'Tools',
+    'description': """
+    Extends the User Roles to enable assiging several users to a role, at the same time, via the use of a server action.
+    """,
+    #'sequence': '1'
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-account/base_user_role_mass_addition',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-account',
+    'depends': ['base_user_role','base'],
+    'data': [
+        'views/base_user_role_mass_addition_wizard.xml',
+        'views/res_user.xml',
+        'data/ir.actions.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
