436f6cfe30ddca9c9219f81c15cfe91255b52cfb
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-09-22 14:40:15 +0200

    Update __manifest__.py

--- partner_portal_user/__manifest__.py
+++ partner_portal_user/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 {
-    'name': 'Partner Portal User',
+    'name': 'Base: Partner Portal User',
     'version': '14.0.0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds a field on partners that shows if the partner is connected to a portal user.',
