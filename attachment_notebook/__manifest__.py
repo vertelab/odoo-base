876d68d637f6b33cc3eadcca15dc36a0de19eeeb
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 09:46:03 +0200

    Update __manifest__.py

--- attachment_notebook/__manifest__.py
+++ attachment_notebook/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Base: Attachment Notebook',
-    'version': '14.0.1',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.1',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds a notebook for other modules to populate with more functionality.',
     'category': 'CRM',
     'description': """
