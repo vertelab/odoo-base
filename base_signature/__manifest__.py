9caa4c41661c40f5d0759d3c38f7c5debfa585e9
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 11:30:07 +0200

    Update __manifest__.py

--- base_signature/__manifest__.py
+++ base_signature/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Base: Signature',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Allows signature to be used anywhere in odoo.',
     'category': 'Technical',
