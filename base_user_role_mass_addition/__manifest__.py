2694fd90d32df39ebec5f8d2450bdcb13390647c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 09:56:51 +0200

    Update __manifest__.py

--- base_user_role_mass_addition/__manifest__.py
+++ base_user_role_mass_addition/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Base: User Roles Mass addition',
-    'version': '14.0.0.1.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.0.1.0',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Extends the User Roles to enable assiging several users to a role, at the same time.',
     'category': 'Tools',
     'description': """
