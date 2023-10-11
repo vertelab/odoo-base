bf65a3d6b0e6f7d40b1811a5de55f740085e6430
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 11:43:19 +0200

    Update __manifest__.py

--- partner_legacy_id/__manifest__.py
+++ partner_legacy_id/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Base: Partner Legacy ID',
-    'version': '14.0.1.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.1.0.0',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
     'category': 'Website',
     'description': """
