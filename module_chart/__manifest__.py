ae6c7643607b1723fd94ffc9d7089f46eee40794
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 11:36:52 +0200

    Update __manifest__.py

--- module_chart/__manifest__.py
+++ module_chart/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Base: Module Chart',
-    'version': '14.0.1.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.1.0.0',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds a graphical description of the Odoo database dependencies.',
     'category': 'Tools',
     'description': """
