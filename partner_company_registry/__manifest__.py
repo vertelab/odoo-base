6b2f8b8b5db94617a1746dd32b9d2a9ed6c76f96
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 11:37:26 +0200

    Update __manifest__.py

--- partner_company_registry/__manifest__.py
+++ partner_company_registry/__manifest__.py
@@ -19,8 +19,8 @@
 ##############################################################################
 {
     'name': 'Base: Partner Company Registry',
-    'version': '14.0.0.0.1',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.0.0.1',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds a Partner Registry field to the partner object.',
     'category': 'CRM',
     'description': """
