f494c71a192df4818347b636bbc42ccebc1d95f3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 11:36:26 +0200

    Update __manifest__.py

--- ir_ui_menu_sort_l10n/__manifest__.py
+++ ir_ui_menu_sort_l10n/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Base: Menu Sort',
-    'version': '14.0.0.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.0.0.0',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Sort menus by name',
     'category': 'Technical',
     'description': """
