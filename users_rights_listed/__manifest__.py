9c36486921718b244a726e1191185202b8d3c018
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 11:47:06 +0200

    Update __manifest__.py

--- users_rights_listed/__manifest__.py
+++ users_rights_listed/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Base: Users Rights Listed',
-    'version': '14.0.0.0.1',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.0.0.1',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds a UserGroup field to the res_users tree-view in debug-mode.',
     'category': 'Technical',
     'description': """
