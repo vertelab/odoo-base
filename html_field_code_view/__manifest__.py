2f47f5e2f7da9bc2be6565038aa1d57b6a574e39
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 11:36:02 +0200

    Update __manifest__.py

--- html_field_code_view/__manifest__.py
+++ html_field_code_view/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Base: Show Code View on HTML Field',
-    'version': '14.0.0.1.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.0.1.0',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Show Code View on HTML Field',
     'category': 'Accounting',
     'description': """
