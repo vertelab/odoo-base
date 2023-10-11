ac40944218915b933196de524a69db35cd778faa
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 10:47:04 +0200

    Update __manifest__.py

--- base_map/__manifest__.py
+++ base_map/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Base: Map',
-    'version': '16.0.1.0.0',
+    'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Allows for quick mapping of Odoo fields to fields in other systems.',
     'category': 'Technical',
