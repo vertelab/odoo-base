38bbf63b7f928faf83fcb6f79800ec4a9268cb00
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 09:55:05 +0200

    Update __manifest__.py

--- base_sftp/__manifest__.py
+++ base_sftp/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Base: SFTP',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'summary': 'Access your documents via SFTP.',
     'category': 'Technical',
     'description': """
