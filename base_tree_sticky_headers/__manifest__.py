8881ddaa600e53377bc7ba69e673f51fc0e98d5d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 11:30:38 +0200

    Update __manifest__.py

--- base_tree_sticky_headers/__manifest__.py
+++ base_tree_sticky_headers/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Base: Tree Sticky Headers ',
-    'version': '14.0.0.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.0.0.0.0',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Sticky Tree Header',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
