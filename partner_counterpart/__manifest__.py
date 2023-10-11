f7a6523b22f226a57453ca232c1dff7a572051ea
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-10-11 11:40:28 +0200

    Update __manifest__.py

--- partner_counterpart/__manifest__.py
+++ partner_counterpart/__manifest__.py
@@ -21,8 +21,8 @@
 
 {
     'name': 'Base: Partner Counterpart',
-    'version': '14.1.0.0.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'version': '16.1.0.0.0',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Partner Counterpart.',
     'category': 'CMS',
     "description": """
