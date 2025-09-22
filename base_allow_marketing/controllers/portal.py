741735e2bcfb390be334bac6c06d489b74d28847
iammiracle <miracleaayodele@gmail.com>
2025-09-22 13:56:55 +0200

    add customizable class to booking form

--- base_allow_marketing/controllers/portal.py
+++ base_allow_marketing/controllers/portal.py
@@ -3,10 +3,11 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 class CustomerPortalExtended(CustomerPortal):
 
-    def _get_mandatory_fields(self):
+    # def _get_mandatory_fields(self):
+    def _get_optional_fields(self):
         """ This method is there so that we can override the mandatory fields """
-        mandatory_fields = super()._get_mandatory_fields()
-        mandatory_fields.append("allow_email_marketing")
-        return mandatory_fields
+        optional_fields = super()._get_optional_fields()
+        optional_fields.append("allow_email_marketing")
+        return optional_fields
 
 
--- partner_gender/controllers/main.py
+++ partner_gender/controllers/main.py
@@ -6,18 +6,29 @@ from requests import post
 
 
 class CustomerPortalInherit(CustomerPortal):
-    SKS_MANDATORY_BILLING_FIELDS = [
-        "name", "phone", "email", "street", "city", "country_id", "gender"]
-    SKS_OPTIONAL_BILLING_FIELDS = [
-        "zipcode", "state_id", "vat", "company_name", "gender_txt"]
+    # SKS_MANDATORY_BILLING_FIELDS = [
+    #     "name", "phone", "email", "street", "city", "country_id", "gender"]
+    # SKS_OPTIONAL_BILLING_FIELDS = [
+    #     "zipcode", "state_id", "vat", "company_name", "gender_txt"]
 
-    @route(['/my/account'], type='http', auth='user', website=True)
-    def account(self, redirect=None, **post):
-        self.MANDATORY_BILLING_FIELDS.extend([x for x in self.SKS_MANDATORY_BILLING_FIELDS if x not in self.MANDATORY_BILLING_FIELDS])
-        self.OPTIONAL_BILLING_FIELDS.extend([x for x in self.SKS_OPTIONAL_BILLING_FIELDS if x not in self.OPTIONAL_BILLING_FIELDS])
-        return super(CustomerPortalInherit, self).account(redirect, **post)
+    # def _get_mandatory_fields(self):
+    #     mandatory_fields = super()._get_mandatory_fields()
+    #     mandatory_fields.append("gender")
+    #     return mandatory_fields
 
-    def details_form_validate(self, data):
-        self.MANDATORY_BILLING_FIELDS.extend([x for x in self.SKS_MANDATORY_BILLING_FIELDS if x not in self.MANDATORY_BILLING_FIELDS])
-        self.OPTIONAL_BILLING_FIELDS.extend([x for x in self.SKS_OPTIONAL_BILLING_FIELDS if x not in self.OPTIONAL_BILLING_FIELDS])
-        return super(CustomerPortalInherit, self).details_form_validate(data)
+    def _get_optional_fields(self):
+        optional_fields = super()._get_optional_fields()
+        # optional_fields.append("gender_txt")
+        optional_fields.extend(["gender_txt", "gender"])
+        return optional_fields
+
+    # @route(['/my/account'], type='http', auth='user', website=True)
+    # def account(self, redirect=None, **post):
+    #     self.MANDATORY_BILLING_FIELDS.extend([x for x in self.SKS_MANDATORY_BILLING_FIELDS if x not in self.MANDATORY_BILLING_FIELDS])
+    #     self.OPTIONAL_BILLING_FIELDS.extend([x for x in self.SKS_OPTIONAL_BILLING_FIELDS if x not in self.OPTIONAL_BILLING_FIELDS])
+    #     return super(CustomerPortalInherit, self).account(redirect, **post)
+
+    # def details_form_validate(self, data):
+    #     self.MANDATORY_BILLING_FIELDS.extend([x for x in self.SKS_MANDATORY_BILLING_FIELDS if x not in self.MANDATORY_BILLING_FIELDS])
+    #     self.OPTIONAL_BILLING_FIELDS.extend([x for x in self.SKS_OPTIONAL_BILLING_FIELDS if x not in self.OPTIONAL_BILLING_FIELDS])
+    #     return super(CustomerPortalInherit, self).details_form_validate(data)
